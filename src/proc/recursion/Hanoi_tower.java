5c2042140ae49b3e2dc35261695e0116f2eda4e2
eulazurko <eulazurko@gmail.com>
2014-02-15 15:31:48 +0200

    Update Hanoi_tower.java

--- src/proc/recursion/Hanoi_tower.java
+++ src/proc/recursion/Hanoi_tower.java
@@ -1,7 +1,8 @@
 package proc.recursion;
 import java.util.Stack;
 
-	
+	/*Проставьте правильные аргументы методов в рекурсивных вызовах exchange. 
+	Итоговый код должен корректно решать задачу о Ханойской башне.*/
 
 	public class Hanoi_tower {
 
@@ -9,14 +10,12 @@ import java.util.Stack;
 	        Stack<Integer> from = new Stack<>();
 	        Stack<Integer> help = new Stack<>();
 	        Stack<Integer> to = new Stack<>();
-	        //from.push(3);
+
 	        from.push(2);
 	        from.push(1);
-	        System.out.println(from.toString());
+
 	        exchange(from, help, to, from.size());
-	        System.out.println("FROM "+from.toString());
-	        System.out.println("HELP "+help.toString());
-	        System.out.println("TO "+to.toString());
+
 	    }
 
 	    public static void exchange(Stack<Integer> from, Stack<Integer> help, Stack<Integer> to, int count) {
